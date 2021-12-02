de54f8a36d7884ad65ea8265768ddaa6cd0d364a
OrShazam <50451273+OrShazam@users.noreply.github.com>
2021-12-02 11:50:14 +0200

    Add files via upload

---
+++ ntdll-scraper/scrap.c
@@ -0,0 +1,40 @@
+
+#include "console.h"
+#include "scrap.h"
+
+void FillScraperData(PScraperData data, const char* dllName){
+	data->hModule = LoadLibraryA(dllName);
+	if (data->hModule == NULL){
+		PrintError("Can't find address of ntdll.\n");
+		return;
+	}
+	MODULEINFO info;
+	DWORD cb = sizeof(MODULEINFO);
+	if (!GetModuleInformation(GetCurrentProcess(),
+		data->hModule,
+		&info, cb)){
+		PrintError("Can't retrieve module information.\n");
+		return;
+	}
+	data->Base = info.lpBaseOfDll;
+	data->ImageSize = info.SizeOfImage;
+}
+
+BOOL FindBytes(PScraperData data, BYTE* bytes, size_t sizeOfBytes, LPVOID* address){
+	BOOL success = FALSE;
+	LPVOID start = data->Base;
+	for (;start < data->Base + data->ImageSize; (BYTE*)start++){
+		if (success) break;
+		for (size_t i = 0; i < sizeOfBytes; i++){
+			if (bytes[i] != (BYTE*)start[i])
+				break;
+			if (i == sizeOfBytes-1)
+				success = TRUE;
+		}
+	}
+	if (success)
+		*address = start;
+	else 
+		*address = NULL;
+	return success;
+}
---
+++ ntdll-scraper/utils.h
@@ -0,0 +1,3 @@
+#include "common.h"
+
+BYTE* HexTranslateSafe(const char* text);
